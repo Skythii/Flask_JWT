a6f0bcf837202bad95204981ae07e907320cd97a
Skythii <tbo2marret@gmail.com>
2025-03-11 15:18:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,7 +29,14 @@ def login():
     if username != "test" or password != "test":
         return jsonify({"msg": "Mauvais utilisateur ou mot de passe"}), 401
 
-    access_token = create_access_token(identity=username, expires_delta: Literal[False])
+    #access_token = create_access_token(identity=username, expires_delta: Literal[False])
+    def create_access_token(
+    identity: username,
+    fresh: Fresh = False,
+    expires_delta: Optional[ExpiresDelta] = False,
+    additional_claims=None,
+    additional_headers=None,
+)
     return jsonify(access_token=access_token)
 
 
