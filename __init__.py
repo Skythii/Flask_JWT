d007368f9b228a34ed10dfe7c8d543846a91135c
Skythii <tbo2marret@gmail.com>
2025-03-11 13:52:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask  #Commentaire de mise à jour
+from flask import Flask  #Commentaire de mise à jour.
 from flask import render_template
 from flask import json
 from flask import jsonify
